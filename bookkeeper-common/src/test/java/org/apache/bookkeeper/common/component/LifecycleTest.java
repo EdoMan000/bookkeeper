9a3bf3c217f389ae20e47ff8aab40ff3d61b6e64
EdoMan000 <manenti000@gmail.com>
2023-05-26 15:29:25 +0200

    badua now working (tested it in bookkeeper-common with stupid LyfecycleTest)

--- bookkeeper-common/src/test/java/org/apache/bookkeeper/common/component/LifecycleTest.java
+++ bookkeeper-common/src/test/java/org/apache/bookkeeper/common/component/LifecycleTest.java
@@ -1,22 +1,15 @@
 package org.apache.bookkeeper.common.component;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+import static org.junit.Assert.*;
 public class LifecycleTest {
-    Lifecycle myLifecycle = mock(Lifecycle.class);
-
-    @Before
-    public void beforeTestLifecycle() {
-        when(myLifecycle.state()).thenReturn(Lifecycle.State.STARTED);
-    }
 
     @Test
-    public void testState() {
-        Assert.assertEquals(myLifecycle.state(), Lifecycle.State.STARTED);
+    public void state() {
+        Lifecycle lifecycle = new Lifecycle();
+        Lifecycle lifecycle2 = new Lifecycle();
+        Assert.assertEquals(lifecycle.state(), lifecycle2.state());
     }
 }
