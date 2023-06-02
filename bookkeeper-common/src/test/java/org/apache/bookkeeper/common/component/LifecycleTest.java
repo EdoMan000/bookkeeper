104f9e091536fa0098502266b333e09cf26fc2cb
EdoMan000 <manenti000@gmail.com>
2023-06-02 16:06:32 +0200

    COMMIT FOR BACKUP - removed ba-dua... everything broke while trying to fix it :) . Committing working copy of repo with only jacoco reports but with new tests (mockito is working... it broke down before). Next commit will be re-adding ba-dua hopefully. By the way the 'strange' error with jacoco was fixed upgrading to version 0.8.10

--- bookkeeper-common/src/test/java/org/apache/bookkeeper/common/component/LifecycleTest.java
+++ bookkeeper-common/src/test/java/org/apache/bookkeeper/common/component/LifecycleTest.java
@@ -1,10 +1,25 @@
 package org.apache.bookkeeper.common.component;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class LifecycleTest {
+
+    Lifecycle lifecycle = mock(Lifecycle.class);
+    @Before
+    public void before(){
+        when(lifecycle.state()).thenReturn(Lifecycle.State.CLOSED);
+    }
+
+    @Test
+    public void anotherTest() {
+        Assert.assertEquals(lifecycle.state(), Lifecycle.State.CLOSED);
+    }
 
     @Test
     public void moveToStartedTest() {
