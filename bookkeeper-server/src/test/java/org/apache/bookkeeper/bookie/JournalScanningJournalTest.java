dff896eced8fd21883c7615b8bd4f764b7088e3e
EdoMan000 <manenti000@gmail.com>
2023-06-13 23:58:14 +0200

    finished with tests hopefully... decommented buggy test classes so workflow will probably fail because bugs were exposed

--- bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/JournalScanningJournalTest.java
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/bookie/JournalScanningJournalTest.java
@@ -218,7 +218,7 @@ public class JournalScanningJournalTest {
 
      */
 
-    @Test@Ignore
+    @Test
     public void scanJournalTest() throws IOException {
         long actualScanOffset = this.journal.scanJournal(this.journalId, this.journalPos, this.scanner);
 
